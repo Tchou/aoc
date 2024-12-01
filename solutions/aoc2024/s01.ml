ae32312a705c59cf75b508e98764f0bc4e506b49
Kim Nguyễn <kn@lmf.cnrs.fr>
2024-12-01 10:40:38 +0100

    Factorize code in solution 1, 2024.

--- solutions/aoc2024/s01.ml
+++ solutions/aoc2024/s01.ml
@@ -7,28 +7,29 @@ struct
         (i1::acc1, i2::acc2)
       ) ([], [])
 
-  let solve_part1 () =
+  let mk_sol f =
     let l1, l2 = read_input () in
-    let l1 = List.sort compare l1 in
-    let l2 = List.sort compare l2 in
-    let res = List.fold_left2 (fun acc i1 i2 ->
-        acc + (abs (i1 - i2))) 0 l1 l2
-    in
+    let res = f l1 l2 in
     Ansi.(printf "%a%d%a\n" fg green res clear color)
+
+  let solve_part1 () =
+    mk_sol (fun l1 l2 ->
+        List.fold_left2 (fun acc i1 i2 ->
+            acc + (abs (i1 - i2)))
+          0
+          (List.sort compare l1) 
+          (List.sort compare l2))
 
   let solve_part2 () =
-    let l1, l2 = read_input () in
-    let count = Hashtbl.create 16 in
-    let open Syntax in
-    let () = List.iter (fun i2 ->
-        Hashtbl.update count i2
-          (fun c -> Some (1 + (c or 0)))) l2
-    in
-    let res = List.fold_left
-        (Agg.Left.sum (fun i -> i * ((Hashtbl.find_opt count i) or 0))) 0 l1
-    in
-    Ansi.(printf "%a%d%a\n" fg green res clear color)
-
+    mk_sol (fun l1 l2 ->
+        let count = Hashtbl.create 16 in
+        let open Syntax in
+        List.iter (fun i2 ->
+            Hashtbl.update count i2
+              (fun c -> Some (1 + (c or 0)))) l2;
+        List.fold_left
+          (Agg.Left.sum (fun i -> i * ((Hashtbl.find_opt count i) or 0))) 0 l1
+      )
 end
 
 let () = Solution.register_mod (module S)
