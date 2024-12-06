585f2078a92197a5ce772465d5066fa1f5c6c2bc
Kim Nguyễn <kn@lmf.cnrs.fr>
2024-12-06 20:58:56 +0100

    Further optimize 2024 day 6.

--- solutions/aoc2024/s06.ml
+++ solutions/aoc2024/s06.ml
@@ -14,104 +14,84 @@ struct
   let read_input () =
     let start = ref (0,0) in
     let y = ref 0 in
-    let grid = Input.fold_lines (fun acc line ->
-        let () =
-          match String.index_opt line '^' with
-            Some x -> start := (x, !y)
-          | None -> ()
-        in incr y;
-        line::acc
-      ) []
-               |> List.rev
-               |> Array.of_list
+    let grid =
+      Input.fold_lines (fun acc line ->
+          let () =
+            match String.index_opt line '^' with
+              Some x -> start := (x, !y)
+            | None -> ()
+          in incr y;
+          line::acc
+        ) []
+      |> List.rev
+      |> Array.of_list
     in
     !start, grid
+
   let valid grid x y =
     let h = Array.length grid in
     let w = String.length grid.(0) in
     x >= 0 && x < w && y >= 0 && y < h
 
-  let print_grid fmt (grid, visited) =
-    Array.iteri (fun j s ->
-        String.iteri (fun i c ->
-            let c = if visited %? (i, j) then 'X' else c in
-            Format.fprintf fmt "%c" c
-          ) s;
-        Format.fprintf fmt "\n") grid
-
-  let walk start grid =
-    let x, y = start in
-    let visited = ~%[] in
-    let rec loop i j x y =
-      visited.%{x, y} <- ();
+  let visited = ~%[]
+  let explore x y grid =
+    let rec loop x y i j path plen =
+      let path, plen = if visited %? (x, y) then path, plen else begin
+          visited.%{x, y}<-();
+          (x, y, i, j)::path, 1+plen
+        end
+      in
       let xi = x + i in
       let yj = y + j in
-      if not (valid grid xi yj) then begin
-        Format.printf "%a\n%!" print_grid (grid, visited);
-        Hashtbl.length visited
-      end
+      if not (valid grid xi yj) then path, plen
+      else if grid.(yj).[xi] = '#' then
+        loop  x y (-j) i path plen
       else
-        let i, j, x, y =
-          if grid.(yj).[xi] = '#' then
-            -j, i, x, y
-          else i, j,xi, yj
-        in
-        loop i j x y
+        loop xi yj i j path plen
     in
-    loop 0 (-1) x y
+    Hashtbl.clear visited;
+    loop x y 0 (-1) [] 0
 
-  let is_cycle stone start grid =
-    let stone_x, stone_y = stone in
-    let x, y = start in
-    let visited = ~%[] in
-    let rec loop i j x y =
+  let visited = ~%[]
+  let has_cycle xs ys x y i j grid =
+    let rec loop i j x y  =
       let key = x, y, i, j in
-      if visited %? key then true, visited
+      if visited %? key then true
       else begin
         visited.%{key} <- ();
         let xi = x + i in
         let yj = y + j in
-        if not (valid grid xi yj) then false, visited
+        if not (valid grid xi yj) then false
+        else if (xi = xs && yj = ys) || grid.(yj).[xi] = '#' then
+          loop (-j) i x y
         else
-          let i, j, x, y =
-            if (xi = stone_x && yj = stone_y) || grid.(yj).[xi] = '#' then
-              -j, i, x, y
-            else i, j, xi, yj
-          in
-          loop i j x y
+          loop i j xi yj
       end
     in
-    loop 0 (-1) x y
+    Hashtbl.clear visited;
+    loop i j x y
 
-  let count_cycles start grid =
-    let h = Array.length grid in
-    let w = String.length grid.(0) in
-    let clean_path = ~%[] in
-    let dirs = [ (1, 0); (-1, 0); (0, 1);(0, -1)] in
+  let count_cycles x y grid =
+    (* find the normal path *)
+    let path, n = explore x y grid in
+    (* if there is a cycle, it can only be found by puting the stone
+       on one of the position of the normal path (and not on the start position)
+       Also, there is no need start iterating from the start but only from the
+       step before hitting the new stone.
+    *)
     let count = ref 0 in
-    for y = 0 to h - 1 do
-      for x = 0 to w - 1 do
-        if grid.(y).[x] = '.' && (
-            Hashtbl.length clean_path = 0 ||
-            List.exists (fun (i, j) -> clean_path %? (x, y, i, j)) dirs)
-        then
-          let b, path = is_cycle (x, y) start grid in
-          if b then incr count
-          else if Hashtbl.length clean_path = 0 &&
-                  not (List.exists (fun (i, j) -> clean_path %? (x, y, i, j)) dirs)
-          then
-            Hashtbl.iter (fun key () -> clean_path.%{key} <- ()) path
-      done;
-    done;
+    List.iter (fun (xs, ys, i, j) ->
+        if has_cycle xs ys (xs-i) (ys-j) i j grid then incr count;
+      ) (List.tl (List.rev path));
     !count
 
   let solve_part1 () =
-    let start, grid = read_input () in
-    let n = walk start grid in
+    let (x, y), grid = read_input () in
+    let _, n = explore x y grid in
     Ansi.(printf "%a%d%a\n" fg green n clear color)
   let solve_part2 () =
-    let start, grid = read_input () in
-    let n = count_cycles start grid in
+    let (x, y), grid = read_input () in
+    let n = count_cycles x y grid in
     Ansi.(printf "%a%d%a\n" fg green n clear color)
 
 end
