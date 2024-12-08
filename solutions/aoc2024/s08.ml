7f7936b337ef218962bbab5cde47e5c7e91e579d
Kim Nguyễn <kn@lmf.cnrs.fr>
2024-12-08 08:20:45 +0100

    Add solution for day 8, 2024.

---
+++ solutions/aoc2024/s08.ml
@@ -0,0 +1,54 @@
+open Utils
+open Syntax
+module S =
+struct
+  let name = Name.mk "s08"
+  let read_input () =
+    Input.fold_lines (fun acc line -> line :: acc) []
+    |> List.rev
+    |> Array.of_list
+
+  let count_antinodes resonnance grid =
+    let h = Array.length grid in
+    let w = String.length grid.(0) in
+    let rmin,rmax = if resonnance then 0, max h w else 1,1 in
+    let valid (x, y) =
+      x >= 0 && y >= 0 && x < w && y < w
+    in
+    let map = ~%[] in
+    for j = 0 to h - 1 do
+      for i = 0 to w - 1 do
+        let c = grid.(j).[i] in
+        if c <> '.' then map.%{c} <- (i,j)::(map.%?{c} or [])
+      done;
+    done;
+    let unique_pos = ~%[] in
+    Hashtbl.iter (fun _ l ->
+        let coords = Array.of_list l in
+        for i = 0 to Array.length coords - 1 do
+          let x1, y1 = coords.(i) in
+          for j = i+1 to Array.length coords - 1 do
+            for r = rmin to rmax do
+              let x2, y2 = coords.(j) in
+              let dx = x2 - x1 in
+              let dy = y2 - y1 in
+              let p1 = (x1 - r * dx, y1 - r * dy) in
+              let p2 = (x2 + r * dx, y2 + r * dy) in
+              if valid p1 then unique_pos.%{p1} <- ();
+              if valid p2 then unique_pos.%{p2} <- ();
+            done
+          done
+        done
+      ) map;
+    Hashtbl.length unique_pos
+  let solve resonnance =
+    let grid = read_input () in
+    let n = count_antinodes resonnance grid in
+    Ansi.(printf "%a%d%a\n" fg green n clear color)
+
+
+  let solve_part1 () = solve false
+  let solve_part2 () = solve true
+end
+
+let () = Solution.register_mod (module S)
