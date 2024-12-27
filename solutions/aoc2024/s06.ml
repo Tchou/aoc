8516d2ae7f6590b2800506b33c35c6873a44c577
Kim Nguyễn <kn@lmf.cnrs.fr>
2024-12-27 09:03:58 +0100

    Fix dune file to remove (ocamlopt_flags) stanzas that shadow those defined in dune-workspace file.

--- solutions/aoc2024/s06.ml
+++ solutions/aoc2024/s06.ml
@@ -39,35 +39,42 @@ struct
   let has_cycle visited pos_s pos idir grid =
     let rec loop id p  =
       let x, y = p in
-      (visited.(y).(x) lsr id) land 1 = 1 || begin
+      (Array.(unsafe_get (unsafe_get visited y) x) lsr id) land 1 = 1 || begin
         visited.(y).(x) <- visited.(y).(x) lor (1 lsl id);
         let p' = Grid.(p +! tdir.(id)) in
-        if not (G.inside grid p') then false
-        else if p' = pos_s || grid.G.!(p') = '#' then
-          loop ((id + 1) land 0b11) p
-        else
-          loop id p'
+        if G.inside grid p'
+        then if p' = pos_s || grid.G.!(p') = '#' then
+            loop ((id + 1) land 0b11) p
+          else
+            loop id p'
+        else false
       end
     in
-    for i = 0 to Array.length visited - 1 do
-      Array.fill visited.(i) 0 (Array.length visited.(i)) 0;
+    for j = 0 to Array.length visited - 1 do
+      let line = Array.unsafe_get visited j in
+      for i = 0 to Array.length line - 1 do
+        Array.unsafe_set line i (0b10000 land (Array.unsafe_get line i))
+      done;
     done;
     loop idir pos
 
-  let count_cycles pos grid =
+  let count_cycles start grid =
     (* find the normal path *)
-    let path, n = explore pos grid in
+    let path, n = explore start grid in
     (* if there is a cycle, it can only be found by puting the stone
        on one of the position of the normal path (and not on the start position)
        Also, there is no need start iterating from the start but only from the
        step before hitting the new stone.
     *)
-    let count = ref 0 in
     let visited = Array.init (G.height grid) (fun _ -> Array.make (G.width grid) 0) in
-    List.iter (fun (pos_s, dir) ->
-        if has_cycle visited pos_s Grid.(pos_s +! (opposite dir)) (idx_dir dir) grid then incr count;
-      ) (List.tl (List.rev path));
-    !count
+    List.fold_left (fun acc (pos_s, dir) ->
+        let x, y = pos_s in
+        if visited.(y).(x) land 0b10000 == 0 then begin
+          visited.(y).(x) <- 0b10000;
+          acc + (int_of_bool (pos_s <> start && (has_cycle visited pos_s Grid.(pos_s +! (opposite dir)) (idx_dir dir) grid
+                                                )))
+        end else acc
+      ) 0 path
 
   let solve_part1 () =
     let start, grid = read_input () in
