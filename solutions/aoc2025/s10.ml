18a0322302eb7e7c4bc664b2c1830e68d2054fb4
Kim Nguyễn <kn@lmf.cnrs.fr>
2025-12-13 09:02:16 +0100

    .

--- solutions/aoc2025/s10.ml
+++ solutions/aoc2025/s10.ml
@@ -2,15 +2,14 @@ open Utils
 module S =
 struct
 
-  type t = { target : int;
-             length : int;
-             len_mask : int;
-             buttons : int array;
+  type t = { lights : int;
+             length : int; (* size of lights and joltages *)
+             buttons : string array;
              joltages : int array;
            }
 
   let set_bit w i = w lor (1 lsl i)
-  let read_target s =
+  let read_lights s =
     let length = String.length s - 2 in
     let acc = ref 0 in
     for i = 1 to length do
@@ -19,7 +18,7 @@ struct
     done;
     !acc, length
 
-  let read_buttons l =
+  let read_buttons len l =
     let rec loop l acc =
       match l with
         [] -> assert false
@@ -28,7 +27,11 @@ struct
         loop ll 
           ((String.sub b 1 (String.length b - 2)
             |> String.split_on_char ','
-            |> List.fold_left (fun acc i -> set_bit acc (int_of_string i)) 0)::acc)
+            |> List.map (fun s -> 
+                int_of_string s
+                |> Char.chr)
+            |> String.implode)::acc)
+
     in
     loop l []
 
@@ -39,18 +42,28 @@ struct
     |> Array.of_list
 
   let read_line l =
-    let target, length = read_target (List.hd l) in 
-    let len_mask = (1 lsl length) - 1 in
-    let buttons, j_str = read_buttons (List.tl l) in
+    let lights, length = read_lights (List.hd l) in 
+    let buttons, j_str = read_buttons length (List.tl l) in
     let joltages = read_joltages j_str in
-    { target; length; len_mask; buttons; joltages}
+    { lights; length; buttons; joltages}
 
   let read_input () =
     Input.list_fields ' ' read_line
 
-  let apply_button lights mask button =
-    (lights lxor button) land mask
+  let apply_button lights button =
+    (* 0 0 -> 0
+       0 1 -> 1
+       1 0 -> 1
+       1 1 -> 0
 
+       String.fold_left (fun acc c -> 
+        acc lxor (1 lsl (Char.code c))) lights button
+    *)
+    let res = ref lights in
+    for i = 0 to String.length button - 1 do
+      res := !res lxor  (1 lsl (Char.code (String.unsafe_get button i)));
+    done;
+    !res
   let find_target machine =
     let open Syntax in
     let queue = Queue.create () in
@@ -59,11 +72,11 @@ struct
     let rec loop () =
       if Queue.is_empty queue then assert false;
       let current, steps = Queue.take queue in
-      if machine.target = current then steps
+      if machine.lights = current then steps
       else
         begin
           Array.iter (fun b -> 
-              let n = apply_button current machine.len_mask b in
+              let n = apply_button current b in
               if not (visited %? n) then begin
                 visited.%{n} <- ();
                 Queue.add (n, steps+1) queue;
@@ -94,15 +107,10 @@ struct
     !mx
   let has_neg j =  Array.exists (fun x -> x < 0) j
 
-  let bits_set v =
-    let rec loop v c =
-      if v = 0 then c 
-      else loop (v land (v-1)) (c+1)
-    in loop v 0
   let pp fmt a = 
-    Format.fprintf fmt "[";
+    Format.fprintf fmt "{";
     Array.iter (Format.fprintf fmt "%d ") a;
-    Format.fprintf fmt "]"
+    Format.fprintf fmt "}"
 
   let pp_b fmt b =
     Format.fprintf fmt "(";
@@ -111,23 +119,14 @@ struct
     done;
     Format.fprintf fmt ")"
 
-  let apply_joltage_button ?(n=1) b jolts =
+  let apply_joltage_button ?(n=1) (b:string) jolts =
 
     let r = Array.copy jolts in
-    for i = 0 to Array.length jolts - 1 do
-      if is_set b i then
-        r.(i) <- r.(i) - n;
+    for i = 0 to String.length b - 1 do
+      let idx = Char.code (String.unsafe_get b i) in
+      r.(idx) <- r.(idx) - n;
     done;
     r
-
-  let bit_exists f w =
-    let rec loop w i = 
-      if w = 0 then false
-      else
-      if w land 1 = 1 then f i else
-        loop (w lsr 1) (i+1)
-    in
-    loop w 0
 
   let diff_zeroes prev next =
     let zeroes = ref [] in
@@ -136,16 +135,19 @@ struct
     done;
     !zeroes
 
+  let has_joltage button pos =
+    String.contains button (Char.chr pos)
+
   let dfs machine =
     let min_steps = ref max_int in
     let buttons = machine.buttons |> Array.to_list in
     let sort = List.sort (fun (_, la) (_, lb) -> Int.compare (List.length la) (List.length lb) ) in
     let buttons_by_jolt = 
       machine.joltages 
-      |> Array.mapi (fun i _ -> i, List.filter (fun b -> is_set b i) buttons )
+      |> Array.mapi (fun i _ -> i, List.filter (fun b -> has_joltage b i) buttons )
       |> Array.to_list |> sort
     in
-    let prune l j = List.map (fun (k, lk) -> (k, List.filter (fun b -> not (is_set b j) ) lk)) l |> sort in
+    let prune l j = List.map (fun (k, lk) -> (k, List.filter (fun b -> not (has_joltage b j) ) lk)) l |> sort in
     let rec loop current buttons n =
       let remaining = max_joltage current in
       if remaining = 0 then (Format.printf "FOUND => %a %d\n%!" pp current n;min_steps := n; n)
@@ -153,18 +155,21 @@ struct
       if remaining + n < !min_steps then
         let res =
           match buttons with
-          | (j, [b]) :: _ when bit_exists (fun k -> k <> j && current.(k) < current.(j)) b -> max_int
+          (* current joltage is at 0, remove all buttons that inpact it *)
           | (j, _) :: rem_buttons when current.(j) = 0 -> loop current (prune rem_buttons j) n
+          | (j, []) :: _ -> max_int (* current joltage is non zero but no more buttons, impossible *)
+         
           | (j, (b::buttons_j)) :: rem_buttons ->
-            let next = apply_joltage_button b current in
+            let s = if buttons_j = [] then current.(j) else 1 in
+            let next = apply_joltage_button ~n:s b current in
             let res1 = if has_neg next then max_int else
                 let new_buttons = match diff_zeroes current next with
                     [] -> buttons
                   | l -> List.filter_map (fun (k, lk) ->
-                      let llk = List.filter (fun b -> not (List.exists (fun z -> is_set b z) l)) lk in
+                      let llk = List.filter (fun b -> not (List.exists (has_joltage b) l)) lk in
                       if llk = [] then None else Some (k, llk) 
                     ) buttons |> sort
-                in loop next new_buttons (n+1) 
+                in loop next new_buttons (n+s) 
             in
             min res1 (loop current ((j,buttons_j)::rem_buttons) n)
           | _ -> max_int
@@ -179,7 +184,7 @@ struct
     let total = List.length machines in
     let i = ref 1 in
     machines 
-    |> List.fold_left (fun acc c -> Format.printf "Current sum %d, computing %d/%d\n%!" acc !i total;incr i;acc + dfs c) 0
+    |> List.fold_left (fun acc c -> Format.printf "Current sum %d, computing %d/%d = %a\n%!" acc !i total pp c.joltages;incr i;acc + dfs c) 0
 
   let name = Name.mk "s10"
   let solve_part1 () = 
